692a35e5b3e6340a8fc60cf6d0265b971107db44
MYUNG SUNG <renownkim@gmail.com>
2018-08-27 21:52:08 +0900

    add model class

--- board/views.py
+++ board/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
-def index(request):
+from .models import board 
+
+def index(ListView):
+	model = board
+
+	pageinate_by = 4
+
+
+	def 
+
+
+
+
+
 	return render(request, 'board/index.html')
